22a4ac42e63b07d217f5a6cbdc8f5aa67347dd67
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-08-20 15:52:05 +0200

    improve usability

--- local-loki.sh
+++ local-loki.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-set -x
 
-TMP_ROOT_DIR=/tmp/loki-sos-report
+TMP_ROOT_DIR=/tmp/sos-to-loki
 
 get_mountpoint() {
 	local input="${1}"
@@ -58,8 +57,32 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+echo "Fetching SOS report"
 sos="$(get_mountpoint "${1}")"
-podman kube down ./pod.yaml
-SOS_REPORT_INPUT="${sos}" envsubst <./pod.yaml | podman kube play -
+echo "Path to local SOS report: ${sos}"
 
-echo "Grafana started at http://localhost:3000/explore"
+echo "Starting Grafana+Loki+Promtail stack"
+podman kube down ./pod.yaml >/dev/null
+SOS_REPORT_INPUT="${sos}" envsubst <./pod.yaml | podman kube play - >/dev/null
+
+sos_journal="${sos}/sos_commands/logs/journalctl_--no-pager"
+
+timezone=$(grep 'Local time' "${sos}/date" | tr -s ' ' | rev | cut -d ' ' -f1 | rev)
+local_timezone=$(date +%Z)
+
+first_entry=$(head -n1 "${sos_journal}" | cut -d ' ' -f1-3)
+last_entry=$(tail -n1 "${sos_journal}" | cut -d ' ' -f1-3)
+
+local_first_entry=$(date --date "${first_entry} ${timezone}" +"%b %d %H:%M:%S")
+local_last_entry=$(date --date "${last_entry} ${timezone}" +"%b %d %H:%M:%S")
+
+from=$(date --date "${local_first_entry} ${local_timezone} -5 minutes" +%s%N | cut -b1-13)
+to=$(date --date "${local_last_entry} ${local_timezone} +5 minutes" +%s%N | cut -b1-13)
+
+echo ""
+printf "                 Timezone     First journal entry       Last entry\n"
+printf "SOS              %-12s %-25s %-20s\n" "${timezone}" "${first_entry}" "${last_entry}"
+printf "Local            %-12s %-25s %-20s\n" "${local_timezone}" "${local_first_entry}" "${local_last_entry}"
+printf "URL timestamps   %-12s %-25s %-20s\n" "${local_timezone}" "${from}" "${to}"
+
+echo -e "\nLink to query with journal:\nhttp://localhost:3000/explore?orgId=1&left=%7B%22datasource%22:%22P8E80F9AEF21F6940%22,%22queries%22:%5B%7B%22refId%22:%22A%22,%22expr%22:%22%7Bfilename%3D%5C%22%2Flogs%2Fsos_commands%2Flogs%2Fjournalctl_--no-pager%5C%22%7D%20%7C%3D%20%60%60%22,%22queryType%22:%22range%22,%22datasource%22:%7B%22type%22:%22loki%22,%22uid%22:%22P8E80F9AEF21F6940%22%7D,%22editorMode%22:%22builder%22%7D%5D,%22range%22:%7B%22from%22:%22${from}%22,%22to%22:%22${to}%22%7D%7D"
